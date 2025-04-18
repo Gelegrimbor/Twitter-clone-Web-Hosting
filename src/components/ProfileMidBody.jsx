a12f192f198ecff869418c6dcca62afc38e0a262
Gelegrimbor <joshuas1177@gmail.com>
2025-04-18 12:10:42 +0800

    Initial commit for twitter-clone-5

---
+++ src/components/ProfileMidBody.jsx
@@ -0,0 +1,101 @@
+import { jwtDecode } from "jwt-decode";
+import { useEffect } from "react";
+import { Button, Col, Image, Nav, Row, Spinner } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPostsByUser } from "../features/posts/postsSlice";
+import ProfilePostCard from "./ProfilePostCard";
+
+export default function ProfileMidBody() {
+  const url = "https://pbs.twimg.com/profile_banners/83072625/1602845571/1500x500";
+  const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem("authToken");
+  //   if (token) {
+  //     const decodedToken = jwtDecode(token);
+  //     const userId = decodedToken.id;
+  //     console.log(userId);
+  //     dispatch(fetchPostsByUser(userId));
+  //   }
+  // }, [dispatch]);
+
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.posts);
+  const loading = useSelector((state) => state.posts.loading);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken.id;
+      console.log(userId);
+      dispatch(fetchPostsByUser(userId));
+    }
+  }, [dispatch]);
+
+  return (
+    <Col sm={6} className="bg-light" style={{ border: "1px solid lightgrey" }}>
+      <Image src={url} fluid />
+      <br />
+
+      <Image
+        src={pic}
+        roundedCircle
+        style={{
+          position: "absolute",
+          top: "140px",
+          marginLeft: 15,
+          width: 150,
+          border: "4px solid #F8F9FA",
+        }}
+      />
+
+      <Row className="justify-content-end">
+        <Col xs="auto">
+          <Button className="rounded-pill mt-2" variant="outline-secondary">
+            Edit Profile
+          </Button>
+        </Col>
+      </Row>
+
+      <p
+        className="mt-5"
+        style={{ margin: 0, fontWeight: "bold", fontSize: "15px" }}
+      >
+        Haris
+      </p>
+      <p style={{ marginBottom: "2px" }}>@haris.samingan</p>
+      <p>I help people</p>
+      <p>Entrepreneur</p>
+      <p>
+        <strong>271</strong> Following <strong>610</strong> Followers
+      </p>
+
+      <Nav variant="underline" defaultActiveKey="/home" justify>
+        <Nav.Item>
+          <Nav.Link eventKey="/home">Tweets</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="replies">Replies</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="highlights">Highlights</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="media">Media</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="likes">Likes</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      {loading && (
+        <Spinner animation="border" className="ms-3 mt-3" variant="primary" />
+      )}
+
+{Array.isArray(posts) && posts.map((post) => (
+  <ProfilePostCard key={post.id} content={post.content} postId={post.id} />
+))}
+    </Col>
+  );
+}
