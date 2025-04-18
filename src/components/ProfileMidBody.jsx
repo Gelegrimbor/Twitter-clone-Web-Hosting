241273fc5f7e8c069d164fb67979721885d050c9
Gelegrimbor <joshuas1177@gmail.com>
2025-04-18 12:26:19 +0800

    Update post components and Redux slice for improved post handling

--- src/components/ProfileMidBody.jsx
+++ src/components/ProfileMidBody.jsx
@@ -1,37 +1,24 @@
-import { jwtDecode } from "jwt-decode";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { Button, Col, Image, Nav, Row, Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPostsByUser } from "../features/posts/postsSlice";
+import { AuthContext } from "./AuthProvider";
 import ProfilePostCard from "./ProfilePostCard";
 
 export default function ProfileMidBody() {
-  const url = "https://pbs.twimg.com/profile_banners/83072625/1602845571/1500x500";
-  const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem("authToken");
-  //   if (token) {
-  //     const decodedToken = jwtDecode(token);
-  //     const userId = decodedToken.id;
-  //     console.log(userId);
-  //     dispatch(fetchPostsByUser(userId));
-  //   }
-  // }, [dispatch]);
+  const url =
+    "https://pbs.twimg.com/profile_banners/83072625/1602845571/1500x500";
+  const pic =
+    "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
 
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts.posts);
   const loading = useSelector((state) => state.posts.loading);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      const userId = decodedToken.id;
-      console.log(userId);
-      dispatch(fetchPostsByUser(userId));
-    }
-  }, [dispatch]);
+    dispatch(fetchPostsByUser(currentUser.uid));
+  }, [dispatch, currentUser]);
 
   return (
     <Col sm={6} className="bg-light" style={{ border: "1px solid lightgrey" }}>
@@ -49,7 +36,6 @@ export default function ProfileMidBody() {
           border: "4px solid #F8F9FA",
         }}
       />
-
       <Row className="justify-content-end">
         <Col xs="auto">
           <Button className="rounded-pill mt-2" variant="outline-secondary">
@@ -57,7 +43,6 @@ export default function ProfileMidBody() {
           </Button>
         </Col>
       </Row>
-
       <p
         className="mt-5"
         style={{ margin: 0, fontWeight: "bold", fontSize: "15px" }}
@@ -70,7 +55,6 @@ export default function ProfileMidBody() {
       <p>
         <strong>271</strong> Following <strong>610</strong> Followers
       </p>
-
       <Nav variant="underline" defaultActiveKey="/home" justify>
         <Nav.Item>
           <Nav.Link eventKey="/home">Tweets</Nav.Link>
@@ -88,14 +72,12 @@ export default function ProfileMidBody() {
           <Nav.Link eventKey="likes">Likes</Nav.Link>
         </Nav.Item>
       </Nav>
-
       {loading && (
         <Spinner animation="border" className="ms-3 mt-3" variant="primary" />
       )}
-
-{Array.isArray(posts) && posts.map((post) => (
-  <ProfilePostCard key={post.id} content={post.content} postId={post.id} />
-))}
+      {posts.map((post) => (
+        <ProfilePostCard key={post.id} post={post} />
+      ))}
     </Col>
   );
 }
